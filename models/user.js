08fab3a13959bea86c8ec41e79466108f72c9cc5
Soaniaina <solosoaniaina4709@gmail.com>
2023-01-23 22:45:59 +0300

    feat: add an property to a user's model

--- models/user.js
+++ models/user.js
@@ -6,6 +6,7 @@ const userSchema = new Schema({
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true, minlength: 4 },
+    profile: { type: String, required: false },
     oauth: { type : Boolean, required: true, default: false}
 });
 
